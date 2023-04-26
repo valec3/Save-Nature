5aa14024a9dc5079c416a813a3f9c9a75dc2b5a8
Victor Raul <v1c70rotakmer@gmail.com>
2023-04-25 22:57:09 -0500

    Terminar seccion posts

--- js/main.js
+++ js/main.js
@@ -3,17 +3,12 @@ const navbar = document.querySelector(".navbar");
 
 
 menu.addEventListener("click",(event)=>{
-
     menu.classList.toggle("fa-times"); //Menu hamburguesa
     navbar.classList.toggle("nav-toggle");
-
 })
 
 window.addEventListener("scroll",(event)=>{
     // Remove menu scrolling
     menu.classList.remove("fa-times"); //Menu hamburguesa
     navbar.classList.remove("nav-toggle");
-
-
-
 })
